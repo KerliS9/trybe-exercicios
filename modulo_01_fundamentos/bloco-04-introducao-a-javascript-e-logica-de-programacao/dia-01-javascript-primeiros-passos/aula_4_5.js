b2ee48f9840f5148869188bc0b4830805f7172c5
Kerli <kerlischroeder9@gmail.com>
2021-11-06 18:46:56 -0300

    feat exercicio 1

---
+++ modulo_01_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/aula_4_5.js
@@ -0,0 +1,16 @@
+//exercicio 1
+const a = 3;
+const b = 2;
+
+let somar = a + b;
+let subtrair = b - a;
+let multiplicar = a * b;
+let dividir = 3 / 2;
+let modular = (3 + 2)% 4;
+console.log (somar);
+console.log (subtrair);
+console.log (multiplicar);
+console.log (dividir);
+console.log (modular);
+
+//exercicio 2
