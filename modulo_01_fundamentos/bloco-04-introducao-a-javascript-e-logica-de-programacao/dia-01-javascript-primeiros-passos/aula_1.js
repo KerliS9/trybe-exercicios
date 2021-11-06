16b9b6e4fd070567f62d3a600ff92ed2e24d4ff3
Kerli <kerlischroeder9@gmail.com>
2021-11-06 14:54:51 -0300

    feat primeiros testes do js no vscode

--- modulo_01_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/aula_1.js
+++ modulo_01_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/aula_1.js
@@ -1,2 +1,9 @@
 const myName = "Kerli";
-console.log (myName);
+const birthCity = "Itapiranga";
+let birthYear = 1987;
+console.log (myName, birthCity, birthYear);
+birthYear = 2030;
+console.log (birthYear);
+birthCity = "Palhoça";
+console.log (birthCity);
+// birthCity é uma constante que não permite alterações.
