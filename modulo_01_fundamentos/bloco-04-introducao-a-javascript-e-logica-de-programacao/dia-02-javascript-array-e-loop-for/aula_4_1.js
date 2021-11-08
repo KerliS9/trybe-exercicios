d0d54cd7746698ab0805c0b2dc0f4d013218b2f4
Kerli <kerlischroeder9@gmail.com>
2021-11-08 15:43:00 -0300

    feat for

---
+++ modulo_01_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/aula_4_1.js
@@ -0,0 +1,17 @@
+//parte 1
+let cars = ['Saab', 'Volvo', 'BMW'];
+console.log(cars[0]); // Saab
+console.log(cars[1]); // Volvo
+console.log(cars[2]); // BMW
+
+//parte 2
+let cars = ["Saab", "Volvo", "BMW"];
+
+for (let index = 0; index < cars.length; index += 1) {
+  console.log(cars[index]);
+}
+
+//EXERCICIO
+//exercicio 1
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+console.log(groceryList);
