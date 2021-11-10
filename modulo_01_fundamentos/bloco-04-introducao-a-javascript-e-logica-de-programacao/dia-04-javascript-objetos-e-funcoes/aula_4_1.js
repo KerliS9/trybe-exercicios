13c63c1ff20dd8e654f69c0bf9eacad11345ef84
Kerli <kerlischroeder9@gmail.com>
2021-11-10 19:22:37 -0300

    feat for/in - exercicios

--- modulo_01_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/aula_4_1.js
+++ modulo_01_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/aula_4_1.js
@@ -29,4 +29,23 @@ for (let value of food) {
 };
 
 //exercicio 1
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge' 
+};
 
+for(let key in names){
+	console.log('Olá ' + names[key]);
+}
+
+//exercicio 2
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for(let key in car) {
+	console.log(key, car[key]);
+}
