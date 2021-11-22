440b525dbd019aef4d0f43cdc9be7482ed41fd86
Kerli <kerlischroeder9@gmail.com>
2021-11-22 12:57:18 -0300

    feat exercicios do course e do dia

---
+++ modulo_01_fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/aula_5_1.js
@@ -0,0 +1,12 @@
+document.querySelector("#header-container").style.backgroundColor = "green";
+document.querySelector(".emergency-tasks").style.backgroundColor = "red";
+document.querySelector(".no-emergency-tasks").style.backgroundColor = "blue";
+
+document.querySelector("#one").style.backgroundColor = "pink";
+document.querySelector("#second").style.backgroundColor = "orange";
+document.querySelector("#third").style.backgroundColor = "gray";
+document.querySelector("#forth").style.backgroundColor = "green";
+
+
+
+
