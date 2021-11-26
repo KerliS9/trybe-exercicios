25f455ed549cf0c9bd88da4c7f8522d2d50ced5e
Kerli <kerlischroeder9@gmail.com>
2021-11-25 22:34:54 -0300

    feat exercicio do dia - form

---
+++ modulo_01_fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/script.js
@@ -0,0 +1,55 @@
+const estados = { 
+	'AC': 'Acre',
+	'AL': 'Alagoas',
+	'AP': 'Amapá',
+	'AM': 'Amazonas',
+	'BA': 'Bahia',
+	'CE': 'Ceará',
+	'DF': 'Distrito Federal',
+	'ES': 'Espírito Santo',
+	'GO': 'Goiás',
+	'MA': 'Maranhão',
+	'MT': 'Mato Grosso',
+	'MS': 'Mato Grosso do Sul',
+	'MG': 'Minas Gerais',
+	'PA': 'Pará',
+	'PB': 'Paraíba',
+	'PR': 'Paraná',
+	'PE': 'Pernambuco',
+	'PI': 'Piauí',
+	'RJ': 'Rio de Janeiro',
+	'RN': 'Rio Grande do Norte',
+	'RS': 'Rio Grande do Sul',
+	'RO': 'Rondônia',
+	'RR': 'Roraima',
+	'SC': 'Santa Catarina',
+	'SP': 'São Paulo',
+	'SE': 'Sergipe',
+	'TO': 'Tocantins'
+	} 
+
+const state = document.getElementById('state');
+
+function criaOptions() {
+	for (let i in estados) {
+		const option = document.createElement('option');
+		option.setAttribute('value', i);
+		option.innerText = estados[i];
+		state.appendChild(option);
+	}
+}
+criaOptions();
+
+const date = document.getElementById('date');
+function checkDate () {
+	const dateValue = date.value;
+	const dia = parseInt(dateValue.slice(0, 2));
+	const mes = parseInt(dateValue.slice(3, 5));
+	const ano = parseInt(dateValue.slice(6, 10));
+	if ((dia > 0 && dia <= 31) && (mes > 0 && mes <= 12) && (ano > 0)) {
+		return true;
+	} else {
+		return alert('Favor, preencher data no formato dd/mm/aaaa');
+	}
+}
+
