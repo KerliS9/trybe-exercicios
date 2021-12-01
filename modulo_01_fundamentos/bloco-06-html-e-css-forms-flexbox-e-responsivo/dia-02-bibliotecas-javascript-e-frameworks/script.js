fa011617983296181c8f3418149cab03b4b612f3
Kerli <kerlischroeder9@gmail.com>
2021-11-30 23:58:03 -0300

    dev: fazer funçoes

--- modulo_01_fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks/script.js
+++ modulo_01_fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks/script.js
@@ -44,11 +44,11 @@ const estados = {
 const state = document.getElementById('state');
 
 function criaOptions() {
-	for (let i in estados) { // chama a relação de itens que estão no obejto
-		const option = document.createElement('option');
+	for (let i in estados) { // chama a relação de itens que estão no objeto
+		const option = document.createElement('option'); // cria o elemento de escolha.
 		// option.setAttribute('value', i);
-		option.innerText = estados[i];
-		state.appendChild(option);
+		option.innerText = estados[i]; // atribui o texto do objeto conforme índice.
+		state.appendChild(option); // adiciona o option como filho do state.
 	}
 }
 
@@ -57,3 +57,10 @@ function criaOptions() {
 window.onload = function () {
 	criaOptions();
 }
+
+/* 3. Logo abaixo do formulário, crie um botão que:
+Chame uma função JavaScript e interrompa o fluxo automático do form utilizando o preventDefault() . Note que isso vai impedir as validações do HTML ao fazer o submit
+Implemente, agora, no Javascript , as validações que foram pedidas ao longo da montagem do formulário.
+Caso todos os dados sejam válidos, monte uma <div> com o consolidado dos dados que foram inseridos no formulário.
+Caso haja algum dado inválido, mostre em uma <div> uma mensagem de erro. Se o erro for na Data de Início , a mensagem deve ser contextualizada.
+4. Crie um botão Limpar que limpa todos os campos do formulário e a <div> com seu currículo também. */
