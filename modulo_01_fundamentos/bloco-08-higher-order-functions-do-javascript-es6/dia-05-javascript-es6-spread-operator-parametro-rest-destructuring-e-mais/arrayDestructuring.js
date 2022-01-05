9c2743d3c979c1e9e3b20f507e95a2331ff16451
KerliS9 <kerlischroeder9@gmail.com>
2022-01-05 19:19:37 -0300

    feat: exercicios do dia

--- modulo_01_fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/arrayDestructuring.js
+++ modulo_01_fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/arrayDestructuring.js
@@ -20,6 +20,8 @@ console.log(thirdCountry); // China
 console.log(fourthCountry); // Canada
 
 console.log('//---------------');
+
+
 // exercício 1 - Produza o mesmo resultado do código, porém utilizando o array destructuring para recolher a função e a saudação.
 const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
 saudacoes[1](saudacoes[0]); // Olá
@@ -27,6 +29,8 @@ saudacoes[1](saudacoes[0]); // Olá
 // Produza o mesmo resultado acima, porém utilizando array destructuring
 const [saudacao, realizaSaudacao] = saudacoes;
 console.log(realizaSaudacao(saudacao));
+
+
 
 console.log('//---------------');
 // exercício 2 - A seguir, temos alguns valores que estão descritos em variáveis incoerentes. Através da desestruturação de arrays, corrija os valores das variáveis.
