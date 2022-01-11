08ccc1b48fe8ec9a22735fe9ed10dd55f9091f3a
KerliS9 <kerlischroeder9@gmail.com>
2022-01-11 18:55:33 -0300

    dev: testes

--- modulo_01_fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/testesAssincronos.test.js
+++ modulo_01_fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/testesAssincronos.test.js
@@ -1,6 +1,27 @@
-test('Não deveria passar!', () => {
+// test('Não deveria passar!', () => {
+//   setTimeout(() => {
+//     expect(10).toBe(5);
+//     console.log('Deveria falhar!');
+//   }, 500);
+// }); // falso positivo
+
+// test('Não deveria passar!', (done) => {
+//   setTimeout(() => {
+//     expect(10).toBe(5);
+//     console.log('Deveria falhar!');
+//     done();
+//   }, 500);
+// }); // irá falhar com uma mensagem de timeout.
+
+test('Não deveria passar!', (done) => {
   setTimeout(() => {
-    expect(10).toBe(5);
-    console.log('Deveria falhar!');
+    try {
+      expect(10).toBe(5);
+      console.log('Deveria falhar!');
+      done();
+    } catch (error) {
+      done(error); 
+    }
   }, 500);
 });
+// Enquanto o try tenta executar a função no seu escopo com sucesso, o catch "captura" o erro, chamando a callback done
