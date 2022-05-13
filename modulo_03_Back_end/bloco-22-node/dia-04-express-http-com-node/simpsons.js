5f3c8d285c54b65e9843b88993e1498147a43f88
KerliS9 <kerlischroeder9@gmail.com>
2022-05-12 21:25:53 -0300

    feat: exercicios 4 á 7

--- modulo_03_Back_end/bloco-22-node/dia-04-express-http-com-node/simpsons.js
+++ modulo_03_Back_end/bloco-22-node/dia-04-express-http-com-node/simpsons.js
@@ -2,41 +2,46 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+const { getSimpsons, setSimpsons } = require('./funcSimps');
+
 app.use(cors());
 app.use(express.json());
 
-app.get('/ping', function (req, res) {
-  console.log({ message: 'pong' });
-  res.status(200).json({ message: 'retornada'})
+// exercicio 6
+app.get('/simpsons', async function (req, res) {
+  const anime = await getSimpsons();
+  // console.log(anime);
+  if (anime.length > 0) return res.status(200).json(anime);
+  return res.status(500).json({ "message": "Erro"});
 });
 
-/* app.listen(3003, () => {
-  console.log('Aplicação ouvindo na porta 3003');
-}); */
-
-// exercicio 2
-app.get('/hello', function (req, res) {
-  res.status(200).json({ message: 'rota criada'})
+app.listen(3004, () => {
+  console.log('Aplicação ouvindo na porta 3004');
 });
 
-app.post('/hello', function (req, res) {
-  const { name } = req.body;
-  res.status(201).json({ "message": `Hello, ${name}!` });
+// exercicio 7
+app.get('/simpsons/:id', async function (req, res) {
+  try {
+    const simpsons = await getSimpsons();
+    const { id } = req.params;
+    const simpsonId = simpsons.find(({ id }) => id === id);
+    // console.log(simpsonId);
+    if (simpsonId) return res.status(200).json(simpsonId);
+    return res.status(404).json({ message: 'simpson not found' });
+  } catch(err) {
+    return res.status(500).json({ "message": "Erro"});
+  }
 });
 
-app.listen(3003, () => {
-  console.log('Aplicação ouvindo na porta 3003');
-});
-
-// exercicio 3
-app.post('/greetings', function (req, res) {
-  const { name, age } = req.body;
-  if (age > 17) res.status(200).json({ "message": `Hello, ${name}!` })
-  res.status(401).json({ "message": "Unauthorized" });
-});
-
-// exercicio 4
-app.post('/users', function (req, res) {
-  const { name, age } = req.body;
-  res.status(200).json({ "message": `Seu nome é ${name} e você tem ${age} anos de idade!` });
+// exercicio 8
+app.post('/simpsons', async function (req, res) {
+  const simpsons = await getSimpsons();
+  const { id, name } = req.body;
+  // const simpsonId = simpsons.map(( { id }) => id).includes(id);
+  // console.log(simpsonId);
+  // if (simpsonId) return res.status(409).json({ message: 'id already exists' });
+  if (simpsons.map(( { id }) => id).includes(id)) return res.status(409).json({ message: 'id already exists' });
+  simpsons.push({ id, name });
+  await setSimpsons(simpsons);
+  return res.status(204).json({ "message": "Inserido com sucesso"});
 });
