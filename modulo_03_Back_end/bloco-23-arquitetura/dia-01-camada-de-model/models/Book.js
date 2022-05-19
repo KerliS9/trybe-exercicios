e5d51a57b008cbffa1dbbfaa78c3a0566effc00d
KerliS9 <kerlischroeder9@gmail.com>
2022-05-19 16:51:48 -0300

    exercios de inclusão da tabela books

---
+++ modulo_03_Back_end/bloco-23-arquitetura/dia-01-camada-de-model/models/Book.js
@@ -0,0 +1,18 @@
+const connection = require('./connection');
+
+// Converte o nome dos campos de snake_case para camelCase
+const serialize = (bookData) => ({
+	authorId: bookData.author_id,
+	title: bookData.title});
+
+// Busca todas as pessoas autoras do banco.
+const getAll = async () => {
+	const [books] = await connection.execute(
+		'SELECT author_id, title FROM model_example.books;',
+	);
+	return books.map(serialize);
+};
+
+module.exports = {
+	getAll,
+};
--- modulo_03_Back_end/bloco-23-arquitetura/dia-01-camada-de-model/routes/routeAuthor.js
+++ modulo_03_Back_end/bloco-23-arquitetura/dia-01-camada-de-model/routes/routeAuthor.js
@@ -2,11 +2,27 @@ const express = require('express');
 const routeAuthor = express.Router();
 
 const Author = require('../models/Author');
+const Book = require('../models/Book');
+
 
 routeAuthor.get('/authors', async (_req, res) => {
 	const authors = await Author.getAll();
 
 	res.status(200).json(authors);
+});
+
+routeAuthor.get('/books', async (_req, res) => {
+	const books = await Book.getAll();
+
+	res.status(200).json(books);
+});
+
+routeAuthor.get('/books/:id', async (req, res) => {
+	// const { id } = req.query;
+	console.log('req', req);
+	const books = await Book.getAll();
+
+	res.status(200).json(books);
 });
 
 module.exports = routeAuthor;
