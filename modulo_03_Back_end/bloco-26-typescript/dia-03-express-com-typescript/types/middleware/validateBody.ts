e8804487dd908c0999e5e4bf1e230bf906aaf4af
KerliS9 <kerlischroeder9@gmail.com>
2022-07-07 15:18:04 -0300

    feat - construindo middleware

--- modulo_03_Back_end/bloco-26-typescript/dia-03-express-com-typescript/types/middleware/validateBody.ts
+++ modulo_03_Back_end/bloco-26-typescript/dia-03-express-com-typescript/types/middleware/validateBody.ts
@@ -1,9 +1,27 @@
 import { NextFunction, Request, Response } from 'express';
+import Joi from 'joi';
 import { StatusCodes } from 'http-status-codes';
-import joiDTO from '../shared/joiValidation';
+import { UserSchema } from '../shared/joiValidation';
+import HttpException from '../shared/httpException';
 // import IJoi from '../interfaces/joiInterface';
 
-export default {
+/* import { convertFromDirectory } from 'joi-to-typescript';
+
+convertFromDirectory({
+  schemaDirectory: './src/schemas',
+  typeOutputDirectory: './src/interfaces',
+  debug: true
+});
+
+import { convertSchema } from 'joi-to-typescript';
+import { UserSchema } from '../shared/joiValidation';
+
+const validateUser = (req: Request, _res: Response, next: NextFunction) => {
+  const resultingInterface = convertSchema({}, UserSchema);
+  resultingInterface?.content = req.body;
+} */
+
+/* export default {
   users: (req: Request, _res: Response, next: NextFunction) => {
     console.log('entrou');
     const validateEntry: string = req.method !== 'PUT' ? req.url.slice(1) : 'update';
@@ -13,4 +31,13 @@ export default {
 
     return next({ status: StatusCodes.BAD_REQUEST, message: error.details[0].message })
   } 
-}
+} */
+
+export const validateObject = (req: Request, _res: Response, next: NextFunction) => {
+  const { error } = UserSchema.validate(req.body);
+  console.log(error)
+  if (error) {
+    // const status = parseInt(error.status);
+    throw new HttpException(error);
+  }
+  };
--- modulo_03_Back_end/bloco-26-typescript/dia-03-express-com-typescript/types/shared/joiValidation.ts
+++ modulo_03_Back_end/bloco-26-typescript/dia-03-express-com-typescript/types/shared/joiValidation.ts
@@ -1,9 +1,7 @@
 import Joi from 'joi';
 
-export default {
-  user: Joi.object().keys({
-    name: Joi.string().min(3).required(),
-    email: Joi.string().email({ minDomainSegments: 2}).required(),
-    password: Joi.string().min(8).max(12).required(),
-  })
-}
+export const UserSchema = Joi.object().keys({
+  name: Joi.string().min(3).required(),
+  email: Joi.string().email({ minDomainSegments: 2}).required(),
+  password: Joi.string().min(8).max(12).required(),
+}).meta({ className: 'User' });
