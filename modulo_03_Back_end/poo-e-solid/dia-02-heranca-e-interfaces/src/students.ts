1d1c3e419ce515ec585408a993c7cf8507cab00c
KerliS9 <kerlischroeder9@gmail.com>
2022-07-12 17:38:13 -0300

    feat - configurante eslint

--- modulo_03_Back_end/poo-e-solid/dia-02-heranca-e-interfaces/src/students.ts
+++ modulo_03_Back_end/poo-e-solid/dia-02-heranca-e-interfaces/src/students.ts
@@ -1,36 +1,47 @@
 class Student {
   private _enrollment: string;
+
   private _name: string;
-  private _examsGrades: number[] = Array();
-  private _worksGrades: number[] = Array();
-  
-  constructor(enrollment: string, name: string, exames: number[], works: number[]) {
+
+  private _examsGrades: number[];
+
+  private _worksGrades: number[];
+
+  constructor(
+    enrollment: string,
+    name: string,
+    exames: number[],
+    works: number[],
+  ) {
     this._enrollment = enrollment;
     this._name = name;
-    this.examsGrades = exames;
-    this.worksGrades = works;
+    this._examsGrades = exames;
+    this._worksGrades = works;
   }
-  
+
   public get enrollment(): string {
     return this._enrollment;
   }
+
   public set enrollment(value: string) {
     this._enrollment = value;
   }
-  
+
   public get name(): string {
     return this._name;
   }
+
   public set name(value: string) {
     this._name = value;
   }
-  
+
   public get examsGrades(): number[] {
     return this._examsGrades;
   }
+
   public set examsGrades(value: number[]) {
     if (value.length !== 4) {
-      throw new Error ('Favor ajustar para quatro notas de exames')
+      throw new Error('Favor ajustar para quatro notas de exames');
     }
     this._examsGrades = value;
   }
@@ -38,19 +49,19 @@ class Student {
   public get worksGrades(): number[] {
     return this._worksGrades;
   }
+
   public set worksGrades(value: number[]) {
     if (value.length !== 2) {
-      throw new Error ('Favor ajustar para duas notas de trabalhos')
+      throw new Error('Favor ajustar para duas notas de trabalhos');
     }
     this._worksGrades = value;
   }
 
   sumGrades(): number {
-    return [...this.examsGrades, ...this.worksGrades]
-    .reduce((prev, grade) => {
-      grade += prev;
-      return grade;
-    }, 0)
+    return [...this.examsGrades, ...this.worksGrades].reduce((prev, grade) => {
+      const g = grade + prev;
+      return g;
+    }, 0);
   }
 
   averageGrades(): number {
@@ -61,11 +72,11 @@ class Student {
 }
 
 const student1 = new Student('123', 'Kerli', [6, 7, 8, 9], [8, 9]);
-console.log('student1: ', student1)
-console.log('sum', student1.sumGrades())
+console.log('student1: ', student1);
+console.log('sum', student1.sumGrades());
 const student2 = new Student('456', 'Mau', [8, 7, 8, 9], [7, 9]);
 console.log('student2: ', student2);
-console.log('average', student1.averageGrades())
+console.log('average', student1.averageGrades());
 // ts-node students.ts
 
 // npm run dev:student
