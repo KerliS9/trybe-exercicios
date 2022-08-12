1025398895a119476018443bbbb666f24d2a25fa
KerliS9 <kerlischroeder9@gmail.com>
2022-08-12 14:49:22 -0300

    feat - create abstract mongoModel

---
+++ modulo_03_Back_end/bloco-30-mongodb-com-node-e-poo/dia-01-mongoose-arquitetura-msc/src/glassesStore/models/mongoModel.ts
@@ -0,0 +1,23 @@
+import IModel from '../interfaces/IModel';
+import { isValidObjectId, Model } from 'mongoose';
+
+abstract class MongoModel<T> implements IModel<T> {
+  // atributos...
+  protected _model:Model<T>;
+
+  constructor(model:Model<T>) {
+    this._model = model;
+  }
+
+  public async create(obj: T): Promise<T> {
+    return this._model.create({ ...obj });
+  }
+
+  public async readOne(_id: string): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    return this._model.findOne({ _id });
+  }
+  // métodos...
+}
+
+export default MongoModel;
