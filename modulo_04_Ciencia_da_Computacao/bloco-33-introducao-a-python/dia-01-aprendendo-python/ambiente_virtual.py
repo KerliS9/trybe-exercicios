e3fcbb5ab5eea4de3fad912a70d7c5694376b455
KerliS9 <kerlischroeder9@gmail.com>
2022-09-15 22:10:10 -0300

    fix - ajustando commits

---
+++ modulo_04_Ciencia_da_Computacao/bloco-33-introducao-a-python/dia-01-aprendendo-python/ambiente_virtual.py
@@ -0,0 +1,10 @@
+# Este comando deve ser executado na raiz do projeto.
+
+# criar ambiente virtual - .venv é nome do ambiente
+# python3 -m venv .venv
+
+# ativar ambiente virtual
+# source .venv/bin/activate
+
+# qual ambiente estou usando
+# which python3
